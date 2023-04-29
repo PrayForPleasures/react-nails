e9869ea022abf219854e30a125430849aeceedb6
PrayForPleasures <dmitriyyarchucK@gmail.com>
2023-04-29 09:52:36 +0300

    r6

--- my-app/src/components/section-one/SectionOne.tsx
+++ my-app/src/components/section-one/SectionOne.tsx
@@ -18,6 +18,16 @@ export const SectionOne = () => {
             <div className="container">
                 <div className="section-one__flex-box">
                     <div className="section-one__left-box">
+                        <div className="section-one__slider-box"></div>
+                        <div className="section-one__main-content">
+                            <h2 className="section-one__main-topic">x Nails</h2>
+                            <p className="section-one__description">«x Nails» - это уютная и комфортная атмосфера для наших гостей, где каждый сможет почувствовать свою исключительность. Мастера салона настоящие эксперты в области совершенствования красоты своих клиентов.</p>
+                            <hr className="section-one__hr"/>
+                            <p className="section-one__description">«x Nails» - это место, где встречаются друзья за чашечкой ароматного чая или чудесного кофе, чтобы отдохнуть от повседневных проблем. Это место, где вас всегда ждут, как старых и добрых друзей.</p>
+                        </div>
+                        <div className="section-one__slider-box"></div>
+                        </div>
+                    <div className="section-one__right-box">
                         <nav className="section-one__nav-soc-links">
                             <a className="section-one__soc-link" href="#"><img className="section-one__soc-link-image" src={vk} alt="ref" /></a>
                             <a className="section-one__soc-link" href="#"><img className="section-one__soc-link-image" src={inst} alt="ref" /></a>
@@ -25,12 +35,7 @@ export const SectionOne = () => {
                             <a className="section-one__soc-link" href="#"><img className="section-one__soc-link-image" src={viber} alt="ref" /></a>
                         </nav>
                     </div>
-                    <div className="section-one__right-box">
-                        <div className="section-one__main-content">
-                            <h2 className="section-one__main-topic">x Nails</h2>
-                            <img className="section-one__main-image" src={line} alt="image" />
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
             <img className="section-one__back-image_right" src={backImage2} alt="image" />=
